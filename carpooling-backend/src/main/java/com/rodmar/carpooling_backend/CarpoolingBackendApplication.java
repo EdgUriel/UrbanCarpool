a4cd8f9c3817d8c487940ca66d5b590a61560764
Edgar Uriel Briseño Rodriguez <edgarurieludg@gmail.com>
2024-09-22 13:08:02 -0600

    Backend base structure for the project

---
+++ carpooling-backend/src/main/java/com/rodmar/carpooling_backend/CarpoolingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rodmar.carpooling_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpoolingBackendApplication.class, args);
+	}
+
+}
---
+++ carpooling-backend/src/test/java/com/rodmar/carpooling_backend/CarpoolingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodmar.carpooling_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
